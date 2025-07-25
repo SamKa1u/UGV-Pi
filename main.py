d986d52af08a1736a3bd335c41317bb6828db88f
Samuel Kalu <samkalu@ttu.edu>
2025-07-24 20:40:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,76 @@
 import cv2
-from VISION.Camera import CameraStream
-from VISION.CV import ObjectTrack
-from config import MODEL_PATH, LABEL_MAP
+from CV import ObjectTrack
+from config import MODEL_PATH
 import threading
+import time
 
 shared_state = {
     "frame": None,
-    "Object": None,
+    "object": {
+        "name" : None,
+        "bbox" : None,
+    },
 }
 
-# --- Start Camera Thread ---
-camera = CameraStream(shared_state)
-camera_thread = threading.Thread(target=camera.run, daemon=True)
-camera_thread.start()
-    
-# --- Start CV Thread ---
-tracker = ObjectTrack(shared_state, model_path=MODEL_PATH)
-tracker_thread = threading.Thread(target=camera.run, daemon=True)
-tracker_thread.start()
+def text_format(label, x1, x2, y2):
+    fnt_scale = 5
+    t_width = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, fnt_scale, 2)[0][0]
+    t_height = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, fnt_scale, 2)[0][1]
+    if t_width < x2-x1 and t_height < y2-(y2-40):
+        return fnt_scale
+    else:
+        while t_width > x2-x1 or t_height > (y2-(y2-40)):
+            fnt_scale *= .85
+            t_width = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, fnt_scale, 2)[0][0]
+            t_height = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, fnt_scale, 2)[0][1]
+        return  fnt_scale
+
+def main():
+    cam = cv2.VideoCapture(0)
+    if not cam.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    while True:
+        ret, frame = cam.read()
+        if ret is None:
+            print('[main_cam]-(!) frame skipped')
+            continue
+        shared_state["frame"] = frame
+
+        if shared_state["object"]["name"] is None:
+            cv2.imshow("frame", frame)
+
+            k = cv2.waitKey(1)
+            if k == 27:
+                break
+
+            time.sleep(.05)
+            continue
+        else:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            label = shared_state["object"]["name"]
+            x1 = shared_state["object"]["bbox"][0]
+            y1 = shared_state["object"]["bbox"][1]
+            x2 = shared_state["object"]["bbox"][2]
+            y2 = shared_state["object"]["bbox"][3]
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255,0,0), 2)
+            cv2.rectangle(frame, (int(x1), int(y2) - 40), (int(x2), int(y2)), (255, 0, 0), -1)
+            fnt_scale = text_format(label, int(x1), int(x2), int(y2))
+            cv2.putText(frame, label, (int(x1), int(y2) - 10), font, fnt_scale, (255, 255, 255), 2)
+            cv2.imshow("frame", frame)
+
+            k = cv2.waitKey(1)
+            if k == 27:
+                break
+            time.sleep(.05)
+
+if __name__ == '__main__':
+    # --- Start CV Thread ---
+    tracker = ObjectTrack(shared_state, model_path=MODEL_PATH)
+    tracker_thread = threading.Thread(target=tracker.run, daemon=True)
+    tracker_thread.start()
+
+
+    # --- Start Camera in main Thread ---
+    main()
