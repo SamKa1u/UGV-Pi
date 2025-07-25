ef8aa6da7c4253fcac76170cbc01a3b7a263b45b
Samuel Kalu <samkalu@ttu.edu>
2025-07-24 20:40:50 -0500

    Update and rename CV to CV.py

--- CV.py
+++ CV.py
@@ -13,28 +13,31 @@ class ObjectTrack:
         # self.model = torch.hub.load("ultralytics/yolov5", "yolov5s")
 
         # load from pt
-        self.model = YOLO("yolov5s.pt")
+        self.model = YOLO("yolov5su.pt")
         
     def run(self):
         while True:
             # retrieve shared frame
             frame = self.shared.get("frame")
-            print("[CV]", frame)
+            # print("[CV]", frame)
             if frame is None:
                 time.sleep(.01)
                 continue
 
             # run model inference
-            results = self.model.predict(frame, conf=.60, imgsz=480, max_det=1, verbose=True)
+            results = self.model.predict(frame, conf=.60, imgsz=480, max_det=1, verbose=False)
             for result in results:
                 boxes = result.boxes.cls.tolist()
+                bbox = result.boxes.xyxy.tolist()
                 if boxes:
                     label = int(boxes[0])
                     obj = LABEL_MAP.get(label, None)
                     if obj:
-                        print(f"[Object Track] Detected: {obj} at: {boxes}")
-                        self.shared["object"] = obj
+                        print(f"[Object Track] Detected: {obj} at: {bbox[0]}")
+                        self.shared["object"]["name"] = obj
+                        self.shared["object"]["bbox"] = bbox[0]
                     else:
-                        self.shared["object"] = None
+                        self.shared["object"]["name"] = None
+                        self.shared["object"]["bbox"] = None
             time.sleep(.5)
 
