eec7e960ca8e5c89ab584d8fb91baa75510c1491
Samuel Kalu <samkalu@ttu.edu>
2025-07-26 04:17:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from picamera2 import Picamera2
-from CV_robo import ObjectTrack
-from config import MODEL_PATH
+from CV_robo import ObjectDetect
+from tracking import ObjectTracker
+from config import x1,x2,y1,y2, OBJECT
 import threading
 import time
 import cv2
@@ -16,10 +17,16 @@ cam.configure(config)
 shared_state = {
 "frame": None,
 "annotated": None,
+"detections": None,
 }
 
 # --- Start CV Thread ---
-tracker = ObjectTrack(shared_state)
+detector = ObjectDetect(shared_state)
+detector_thread = threading.Thread(target=detector.run, daemon=True)
+detector_thread.start()
+
+# --- Start Tracking Thread ---
+tracker = ObjectTracker(shared_state)
 tracker_thread = threading.Thread(target=tracker.run, daemon=True)
 tracker_thread.start()
 
@@ -39,12 +46,18 @@ while True:
     # replace frame with annotated if available
     if annotated is not None:
         frame = annotated
-        
-    cv2.imshow("frame", frame)
+    
+    # add ROI to frame
+    cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,0),3)
+    cv2.putText(frame, "Object of Interest: " + OBJECT, (5,470), cv2.FONT_HERSHEY_SIMPLEX, .75, (255,255,255),2)
+ 
+    # display frame for viewing
+    cv2.imshow("frame", cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
     k = cv2.waitKey(1)
     if k == 27:
         break
         
     time.sleep(.05)
-    
+   
 cv2.destroyAllWindows()
+
