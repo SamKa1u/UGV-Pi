cb98d824d0f3cd78e57aadc7722834b4ef2b6b14
Samuel Kalu <samkalu@ttu.edu>
2025-07-26 04:15:33 -0500

    Update CV_robo.py

--- CV_robo.py
+++ CV_robo.py
@@ -4,32 +4,30 @@ from rfdetr import RFDETRNano
 from rfdetr.util.coco_classes import COCO_CLASSES
 
 
-class ObjectTrack:
+class ObjectDetect:
     def __init__(self, shared):
         self.shared = shared
         self.model = RFDETRNano(pretrain_weights="MODELS/rf-detr-nano.pth")
+#         self.model.optimize_for_inference()
 
     def run(self):
         while True:
-#             print(type(self.model))
-#             print("[CV_robo] Start")
-# 
             frame = self.shared.get("frame")
 #             print("[CV_robo]", frame)
-            
             if frame is None:
                 print("[CV_robo]-! No frame received")
                 continue
             
-            detections = self.model.predict(frame, threshold=0.3)
-            print("[CV_robo]", detections)
+            detections = self.model.predict(frame, threshold=0.5)
+            self.shared["detections"] = detections
+#             print("[CV_robo]", detections)
 
             labels = [
                 f"{COCO_CLASSES[class_id]} {confidence:.2f}"
                 for class_id, confidence
                 in zip(detections.class_id, detections.confidence)
             ]
-            print("[CV_robo] Labels", labels)
+#             print("[CV_robo] Labels", labels)
 
             annotated_frame = frame.copy()
             annotated_frame = sv.BoxAnnotator().annotate(annotated_frame, detections)
